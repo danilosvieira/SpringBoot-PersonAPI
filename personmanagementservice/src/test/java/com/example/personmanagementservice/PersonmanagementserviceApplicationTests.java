a84f67422410d85c70d4914a80396b81e36add27
d.santana.vieira <danilosvieira.upe@gmail.com>
2024-06-06 15:16:37 -0300

    Criação do projeto

---
+++ personmanagementservice/src/test/java/com/example/personmanagementservice/PersonmanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.personmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonmanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
